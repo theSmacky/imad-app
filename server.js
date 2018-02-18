9fc9f319db856699b876860f34d22dd41a460990
Nishant Singh <33595105+theSmacky@users.noreply.github.com>
2018-02-18 17:57:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+app.get('article-one', function(req, res){
+   res.send('Article one is requested and will be served here!'); 
+});
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
